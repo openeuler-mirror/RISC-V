201835460c3998fe2d475b37a7311017cf2c2bb2
Liang Li (Euler) <liliang889@huawei.com>
2021-07-30 18:21:26 +0800

    tools/pull_src.py: fix the simple script
    Signed-off-by: Liang Li (Euler) <liliang889@huawei.com>

--- tools/pull_src.py
+++ tools/pull_src.py
@@ -1,32 +1,49 @@
-#usage:
-# create a directory named src
-# copy this file to src
+#!/usr/bin/env python3
+
+# This tools is a simple helper to pull source of specified pkg
+#
+# usage:
 # execute command:
-#      python3 pull_src.py <path_of_openEuler:Mainline:RISC-V> <the_pacakege_you_want_to_pull>
+#      <path_to_this_repo>/tools/pull_src.py <the_pacakege_you_want_to_pull>
+#
+# eg:
+#      <path_to_this_repo>/tools/pull_src.py bash
+#
+
 import os
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import subprocess
 import sys
 
-def gitee_pull(base, name):
+def _pull(base, name):
     fullname = os.path.join(base, name, "_service")
     DOMTree = xml.dom.minidom.parse(fullname)
     root = DOMTree.documentElement
     services = root.getElementsByTagName("service")
     params = services[0].getElementsByTagName("param")
     url = params[1].childNodes[0].data
-    revision = params[3].childNodes[0].data
-    if len(revision) != 40:
-        subprocess.getstatusoutput("git clone %s -b %s"%(url, revision))
+    revision = params[2].childNodes[0].data
+    print("_url: %s" % url)
+    print("_revision: %s" % revision)
+
+    if revision != ".git":
+        _cmd = "git clone %s -b %s " % (url, revision)
+        print("_cmd: %s" % _cmd)
+        subprocess.getstatusoutput("git clone %s -b %s" % (url, revision))
     else:
-        subprocess.getstatusoutput("git clone %s"%url)
-        subprocess.getstatusoutput("cd %s"%url)
-        subprocess.getstatusoutput("git reset --hard %s"%revision)
-    
-def main():
-    base = os.path.realpath(sys.argv[1])
-    gitee_pull(base, sys.argv[2])
+        _cmd = "git clone %s" % url
+        print("_cmd: %s" % _cmd)
+        subprocess.getstatusoutput("git clone %s" % url)
+        subprocess.getstatusoutput("cd %s" % url)
+        subprocess.getstatusoutput("git reset --hard %s" % revision)
+
+def _main():
+    base = os.path.realpath(sys.argv[0])
+    base = os.path.dirname(base)
+    base = os.path.dirname(base)
+    base = base + "/configuration/obs_meta/openEuler:Mainline:RISC-V"
+    _pull(base, sys.argv[1])
 
 if __name__== '__main__':
-    main()
+    _main()
