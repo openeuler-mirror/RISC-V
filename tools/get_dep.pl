d2e1cb62f6b6e57745a0a5f4053ac1f71d4c62db
Liang Li (Euler) <liliang889@huawei.com>
2021-08-06 17:59:06 +0800

    tools: dep handling: fixed dependency handling then provide dependency query
    Save dependencies into .deps file for later use on dependency quering.
    
    Signed-off-by: Liang Li (Euler) <liliang889@huawei.com>

---
+++ tools/get_dep.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+#use strict;
+
+use Switch;
+use Storable;
+use Data::Dumper;
+
+sub _print_help
+{
+    my ($app) = @_;
+    print "Usage:\n";
+    print $app  . " -h\n";
+    print "  for this help menu.\n";
+    print $app  . "-p app_name\n";
+    print "  for example:\n";
+    print $app  . " -p bash\n";
+    print "  to fetch dependency of package [bash].\n";
+}
+
+#####################################################
+my %pkg_dep_map;
+open DEPS, "<.deps" or die "error reading: $1!";
+{
+    local $/;
+    %pkg_dep_map = % { eval <DEPS> };
+}
+close(DEPS) or die "error closing file: $1!";
+
+switch($ARGV[0]) {
+    case "-h" {
+        _print_help($0);
+        exit(0);
+    }
+    case "-p" {
+        my $pkg = $ARGV[1];
+        if (exists($pkg_dep_map{$pkg})) {
+            print "package: [" . $pkg . "] deps:\n";
+            my @deps = $pkg_dep_map{$pkg};
+            print "@{$pkg_dep_map{$pkg}}\n";
+        } else {
+            print "Please make sure the parse_dep.pl has success. And the " .
+                $pkg . " is in openEuler source repo.\n";
+        }
+    }
+    else {
+        _print_help($0);
+    }
+}
+
+exit(0);
