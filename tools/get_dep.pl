ef632b96787457c7ab3661a70974d429f1ca10be
Liang Li (Euler) <liliang889@huawei.com>
2021-08-11 14:52:22 +0800

    tools/get_dep.pl: add '-r' parameter to parse build-dep recurcively
    Use '-r <pkg>' to find all deps for the 'build farm' of '<pkg>'.
    
    Eg. for 'bash', run 'get_dep.pl -r bash' would get all builddep for
    bash, then we'can leverage these 'builddep' to prepare a build image
    to run 'rpmbuild bash.spec' subsequently.
    
    Signed-off-by: Liang Li (Euler) <liliang889@huawei.com>

--- tools/get_dep.pl
+++ tools/get_dep.pl
@@ -52,6 +52,39 @@ switch($ARGV[0]) {
                 $pkg . " is in openEuler source repo.\n";
         }
     }
+    case "-r" {
+        my $pkg = $ARGV[1];
+        my @alldep;
+        my %depmap;
+        my @queue;
+        my %pkg_info = %{$pkg_dep_map{$pkg}};
+        my @deps = @{$pkg_info{'bdep'}};
+
+        for my $_d (@deps) {
+            $depmap{$_d} = 1;
+            push(@queue, ($_d));
+        }
+
+        while (scalar @queue > 0) {
+            my $h = shift(@queue);
+            push(@alldep, ($h));
+            #print "$h\n";
+            my %pinfo = %{$pkg_dep_map{$h}};
+            for my $nd (@{$pinfo{'bdep'}}) {
+                unless (exists($depmap{$nd})) {
+                    push(@queue, ($nd));
+                    $depmap{$nd} = 1;
+                } else {
+                    $depmap{$nd}++;
+                }
+            }
+        }
+
+        print "\nThere are [ " . scalar @alldep . " ] deps.\n";
+        for my $k (@alldep) {
+            print $k . " -> " . $depmap{$k} . "\n";
+        }
+    }
     else {
         _print_help($0);
     }
