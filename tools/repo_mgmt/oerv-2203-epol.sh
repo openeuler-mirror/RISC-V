e46a95b05ce09a6a1e947e4690a281673d54e161
jchzhou <jchzhou@outlook.com>
2022-05-09 11:55:50 +0800

    feat(repo): add Epol_2203 repo

---
+++ tools/repo_mgmt/oerv-2203-epol.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export working_directory="/var/www"
+export sync_path="openEuler-RISC-V/development/EPOL"
+export sync_name="Epol_22.03"
+export sync_arch="riscv64"
+export log_file="$working_directory/log/$sync_name.log"
+export log_command="|tee -a $log_file"
+export sync_command="reposync -a $sync_arch -r $sync_name -p $working_directory/public/$sync_path/ $log_command"
+export meta_gen_command="createrepo --update $working_directory/public/$sync_path/$sync_name/ $log_command"
+
+bash "$working_directory/sync/script/sync_common.sh"
